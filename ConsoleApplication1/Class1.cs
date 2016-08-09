d6e1203e49bb5844eaca2eb84986c696138d2b41
tgroeneboer <tom.groeneboer@liones.nl>
2016-08-09 11:21:19 +0200

    Initial commit

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Saxon.Api;
+
+namespace ConsoleApplication1
+{
+	class Class1
+	{
+		private static readonly Processor Processor = new Processor();
+
+	}
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Saxon.Api;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		
+
+		static void Main(string[] args)
+		{
+			var ding = new Class1(  );
+		}
+	}
+}
